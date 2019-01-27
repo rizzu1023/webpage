1237c767692043f0a8bb9c47f37d5e8c950a4178
rizzu1023 <mrizwan9022@gmail.com>
2019-01-27 20:09:06 +0530

    myths page complplete

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
     $(".navbarmyths__menu").click(function(){
         $(".navigation").show(400);
         $(".navbarmyths").hide(400);
+        $(".navbar-index").hide(400);
     });
 
     $(".navigation-hide").click(function(){
         $(".navigation").hide(400);
         $(".navbarmyths").show(400);
+        $(".navbar-index").show(400);
+
         
     })
 });
---
+++ js/myths.js
@@ -0,0 +1,67 @@
+
+// console.log(border);
+
+$(document).ready(function(){
+    $('#content2').hide();
+    $('#content3').hide();
+    $('#content4').hide();
+    $('#content5').hide();
+    var bordernone = $('.bnone');
+    bordernone.css('border', 'none');
+
+
+    $('#show1').click(function(){
+        $('#content').toggle(500);
+        var src = ($(this).attr('src') === 'img/down.svg') ? 'img/up.svg' : 'img/down.svg';
+        $(this).attr('src',src);
+        border_show(src,0);
+
+    });
+
+    $('#show2').click(function(){
+        $('#content2').toggle(500);
+        var src = ($(this).attr('src') === 'img/down.svg') ? 'img/up.svg' : 'img/down.svg';
+        $(this).attr('src',src); 
+        border_show(src,1);
+
+    });
+
+    $('#show3').click(function(){
+        $('#content3').toggle(500);
+        var src = ($(this).attr('src') === 'img/down.svg') ? 'img/up.svg' : 'img/down.svg';
+        $(this).attr('src',src); 
+        border_show(src,2);
+
+    });
+
+    $('#show4').click(function(){
+        $('#content4').toggle(500);
+        var src = ($(this).attr('src') === 'img/down.svg') ? 'img/up.svg' : 'img/down.svg';
+        $(this).attr('src',src); 
+        border_show(src,3);
+
+    });
+
+    $('#show5').click(function(){
+        $('#content5').toggle(500);
+        var src = ($(this).attr('src') === 'img/down.svg') ? 'img/up.svg' : 'img/down.svg';
+        $(this).attr('src',src); 
+    });
+
+    
+
+    function border_show(src,tag){
+        // x = document.getElementsByClassName('myths__questions--titile');
+        x = $('.myths__questions--title');
+        style= window.getComputedStyle(x[0],null);
+        border = style.getPropertyValue('border-bottom');
+        if( src === 'img/down.svg'){
+            x[tag].style.borderBottom = "1px solid #707070";
+
+        }
+        else{
+            x[tag].style.borderBottom = "none";
+            
+        }
+    }
+});
