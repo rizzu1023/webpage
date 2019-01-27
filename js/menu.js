297a3891f08ca45a04e3762dcbf1777a4fb250d2
rizzu1023 <mrizwan9022@gmail.com>
2019-01-27 12:39:16 +0530

    navbar complete, some changes

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $(".navbarmyths__menu").click(function(){
-        $(".navigation").toggle(400);
+        $(".navigation").show(400);
+        $(".navbarmyths").hide(400);
     });
+
+    $(".navigation-hide").click(function(){
+        $(".navigation").hide(400);
+        $(".navbarmyths").show(400);
+        
+    })
 });
